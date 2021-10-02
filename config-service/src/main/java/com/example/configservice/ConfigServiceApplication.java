23a16628e8f7e8ca84e684aae610212a32c072d9
phw11 <hwpark11@kakao.com>
2021-10-02 20:24:27 +0900

    feat : config service 환경설정 추가

--- config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.hw.configservice;
+package com.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/example/configservice/ConfigServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hw.configservice;
+package com.example.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

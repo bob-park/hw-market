0da15ef230b19dee6d1c7dfcd161078880686e28
phw11 <hwpark11@kakao.com>
2021-10-02 21:07:19 +0900

    HM-11 Root Project 에 api gateway service 추가

--- config-service/src/main/java/com/hw/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/hw/configservice/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.configservice;
+package com.hw.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

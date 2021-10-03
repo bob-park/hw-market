483ae5921cf8148103b0c7951d87f9a6bb9743c8
phw11 <hwpark11@kakao.com>
2021-10-03 14:38:09 +0900

    HM-25 사용자 테이블 변경 적용

--- user-service/src/main/java/com/hw/userservice/commons/entity/User.java
+++ user-service/src/main/java/com/hw/userservice/commons/entity/User.java
@@ -1,33 +1,43 @@
 package com.hw.userservice.commons.entity;
 
+import com.hw.userservice.commons.entity.base.BaseTimeEntity;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
+import org.checkerframework.common.aliasing.qual.Unique;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends BaseTimeEntity {
 
   @Id @GeneratedValue private Long id;
 
+  @Unique
+  @Column(nullable = false)
   private String userId;
+
+  @Column(nullable = false)
   private String password;
 
+  @Column(nullable = false)
   private String name;
 
+  private String phone;
+
+  private String email;
+
   private User(Builder builder) {
-    this(null, builder.userId, builder.password, builder.name);
+    this(null, builder.userId, builder.password, builder.name, builder.phone, builder.email);
   }
 
-  private User(Long id, String userId, String password, String name) {
+  private User(Long id, String userId, String password, String name, String phone, String email) {
     this.id = id;
     this.userId = userId;
     this.password = password;
     this.name = name;
+    this.phone = phone;
+    this.email = email;
   }
 
   protected User() {}
@@ -52,6 +62,14 @@ public class User {
     return name;
   }
 
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
   @Override
   public String toString() {
     return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
@@ -59,6 +77,8 @@ public class User {
         .append("userId", userId)
         .append("password", password)
         .append("name", name)
+        .append("phone", phone)
+        .append("email", email)
         .toString();
   }
 
@@ -67,6 +87,9 @@ public class User {
     private String userId;
     private String password;
     private String name;
+
+    private String phone;
+    private String email;
 
     public Builder userId(String userId) {
       this.userId = userId;
@@ -80,6 +103,16 @@ public class User {
 
     public Builder name(String name) {
       this.name = name;
+      return this;
+    }
+
+    public Builder phone(String phone) {
+      this.phone = phone;
+      return this;
+    }
+
+    public Builder email(String email) {
+      this.email = email;
       return this;
     }
 

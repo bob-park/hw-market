c0eb6ca7c7cb04b7143b7882304baffc76aab22f
phw11 <hwpark11@kakao.com>
2021-10-03 14:37:52 +0900

    HM-25 password encoder 추가

--- user-service/src/main/java/com/hw/userservice/commons/security/SecurityConfiguration.java
+++ user-service/src/main/java/com/hw/userservice/commons/security/SecurityConfiguration.java
@@ -12,23 +12,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   private final Environment env;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  public SecurityConfiguration(Environment env) {
+  public SecurityConfiguration(Environment env, BCryptPasswordEncoder bCryptPasswordEncoder) {
     this.env = env;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable();
 
-    //    http.authorizeRequests().antMatchers("/users/**").permitAll();
-
     http.authorizeRequests()
         .antMatchers("/actuator/**") // actuator 적용 제외
         .permitAll()
-        .antMatchers("/**").permitAll();
-//        .hasIpAddress(env.getProperty("gateway.ip")) // 해당 IP 가 들어있는 경우만 처리
-//        .and(); // 필터 추가
+        .antMatchers("/**")
+        .permitAll();
+    //        .hasIpAddress(env.getProperty("gateway.ip")) // 해당 IP 가 들어있는 경우만 처리
+    //        .and(); // 필터 추가
 
     // h2-console 을 이용할 경우 반드시 적용해야함
     // frame 으로 구성되어있는 경우 인증때문에 정상적으로 동작안함
